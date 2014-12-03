58ab47f3ace8b651364e7f1acb85b17d0e4a1c1a
AdrianAlan <adrianalan.pol@gmail.com>
2014-12-03 16:45:22 +0100

    bootstraping

--- app/pmp.py
+++ app/pmp.py
@@ -1,34 +1,33 @@
 from flask import Flask
-from flask import render_template
+from flask import make_response
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    app.logger.debug('index opened')
-    #return send_file('templates/index.html')
-    return make_response(open('templates/index.html').read())
+    return make_response(open('app/templates/index.html').read())
 
+@app.route('/dashboard')
+def dashboard():
+    return make_response(open('app/templates/dashboard.html').read())
+
+"""
 @app.route('/about')
 def about():
     return 'About'
 
-@app.route('/get_stats')
-def about():
+@app.route('/getstats')
+def getstats():
     return 'Get Stats'
 
-@app.route('/dashboard')
-def about():
-    return 'Dashboard'
-
-@app.route('/load_stats')
-def about():
+@app.route('/loadstats')
+def loadstats():
     return 'Load Stats'
 
 @app.route('/hello/<idx>/int:<idx2>')
 def hello_world(idx, idx2):
     return 'Hello World!%s%s' % (idx,idx2)
-
+"""
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
