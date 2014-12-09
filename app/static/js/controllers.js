350ebf477cff5ed22172a61845ff4065eddaf986
AdrianAlan <adrianalan.pol@gmail.com>
2014-12-09 11:34:10 +0100

    fixed indentation

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -1,32 +1,45 @@
-function resultsCtrl($scope, $http){
+function resultsCtrl($scope, $http) {
 
-    $scope.allRequestData=[];
+    $scope.allRequestData = [];
 
     $scope.requests = {};
-    $scope.requests.selections=['prepid', 'priority', 'pwg'];
-    $scope.requests.options={grouping:['member_of_campaign'], value:"total_events", stacking:[], coloring:"status" };
-    $scope.requests.settings={duration:1000, legend:true, sort:true};
-    $scope.requests.radio={'scale':["linear", "log"], 'operation':["sum", "count"]};
-    $scope.piecharts = {};
-    $scope.piecharts.sum = "total_events";
-    $scope.piecharts.fullTerms = ["new", "validation", "defined", "approved", "submitted", "done"];
-    $scope.piecharts.compactTerms = ["done", "to do"];
-    $scope.piecharts.nestBy = ["member_of_campaign", "status"];
-    $scope.piecharts.domain = ["new", "validation", "done" , "approved", "submitted", "nothing", "defined", "to do"];
+    $scope.requests.options = {
+        grouping: ['member_of_campaign'],
+        value: "total_events",
+        stacking: [],
+        coloring: "status"
+    };
+    $scope.requests.radio = {
+        'scale': ["linear", "log"],
+        'operation': ["sum", "count"]
+    };
+    $scope.requests.selections = ['prepid', 'priority', 'pwg'];
+    $scope.requests.settings = {
+        duration: 1000,
+        legend: true,
+        sort: true
+    };
 
-    $scope.get_stats = function(query, add){
+    $scope.piecharts = {};
+    $scope.piecharts.compactTerms = ["done", "to do"];
+    $scope.piecharts.domain = ["new", "validation", "done", "approved", "submitted", "nothing", "defined", "to do"];
+    $scope.piecharts.fullTerms = ["new", "validation", "defined", "approved", "submitted", "done"];
+    $scope.piecharts.nestBy = ["member_of_campaign", "status"];
+    $scope.piecharts.sum = "total_events";
+
+    $scope.get_stats = function (query, add) {
         $scope.loadingData = true;
-	var promise = $http.get("api/"+query);
-      promise.then(function(data){
-          if(query!='' && add) {
-            data.data.results.push.apply(data.data.results, $scope.allRequestData);
-          }
-        $scope.loadingData = false;
-        $scope.allRequestData = data.data.results;
-      }, function(){
-        alert("Error getting requests");
-        $scope.loadingData = false;
-    });
+        var promise = $http.get("api/" + query);
+        promise.then(function (data) {
+            if (query !== '' && add) {
+                data.data.results.push.apply(data.data.results, $scope.allRequestData);
+            }
+            $scope.loadingData = false;
+            $scope.allRequestData = data.data.results;
+            console.log(data.data.results)
+        }, function () {
+            alert("Error getting requests");
+            $scope.loadingData = false;
+        });
     };
 }
-
