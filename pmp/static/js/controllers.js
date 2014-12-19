d415a4ccd3e428ce88ab9b4c21d24a120b6f728b
AdrianAlan <adrianalan.pol@gmail.com>
2014-12-19 15:14:34 +0100

    error message field

--- pmp/static/js/controllers.js
+++ pmp/static/js/controllers.js
@@ -1,6 +1,40 @@
 'use strict';
 
 pmpApp.controller('MainController', function($location, $scope, $timeout, $http) {
+    $scope.popUpMessage = '';
+    $scope.showPopUp = function(type, text) {
+        switch (type) {
+            case 'error':
+                $scope.popUpMessage = text;
+                $scope.showError = true;
+                $scope.showWarning = false;
+                $scope.showSuccess = false;
+                $timeout(function() {$scope.showPopUp('', '');}, 2000);
+                break;
+            case "warning":
+                $scope.popUpMessage = text;
+                $scope.showError = false;
+                $scope.showWarning = true;
+                $scope.showSuccess = false;
+                $timeout(function() {$scope.showPopUp('', '');}, 2000);
+                break;
+            case "success":
+                $scope.popUpMessage = text;
+                $scope.showError = false;
+                $scope.showWarning = false;
+                $scope.showSuccess = true;
+                console.log($scope.showSuccess)
+                $timeout(function() {$scope.showPopUp('', '');}, 2000);
+                break;
+            default:
+                $timeout(function() {$scope.popUpMessage = '';}, 1000);
+                $scope.showError = false;
+                $scope.showWarning = false;
+                $scope.showSuccess = false;
+                break;
+        }
+    }
+
     $scope.arrRequestOptionsValues = [1,2,4,0,0,0];
     $scope.arrRequestRadioValues = [0, 0];
     $scope.arrCampaigns = [];
@@ -39,6 +73,7 @@ pmpApp.controller('MainController', function($location, $scope, $timeout, $http)
 });
 
 pmpApp.controller('CampaignsController', function($http, $location, $scope, $timeout) {
+
     $scope.setURL = function(optionName, optionValue){
         if (optionName != '' && optionValue != '') {
             $scope.arrRequestOptionsValues[$scope.arrOptionValues.indexOf(optionValue)] = $scope.arrOptionNames.indexOf(optionName);
@@ -55,39 +90,42 @@ pmpApp.controller('CampaignsController', function($http, $location, $scope, $tim
             $scope.setURL('', '');
         }
     }
+
     $scope.typeOfGraph = "cam"
     $scope.$parent.title = 'Statistics of Campaigns';
     $scope.$parent.allRequestData = [];
     $scope.$parent.piecharts.fullTerms = ["new", "validation", "defined", "approved", "submitted", "done"];
     $scope.load = function(campaign, add) {
-        $scope.loadingData = true;
-        var promise = $http.get("api/" + campaign + "/simple");
-        promise.then(function(data) {
-            if (campaign !== '' && add) {
-                data.data.results.push.apply(data.data.results, $scope.allRequestData);
-                $scope.arrCampaigns.push(campaign);
-            } else {
-                $scope.arrCampaigns = [campaign];
-            }
-            $scope.loadingData = false;
-            $scope.$parent.allRequestData = data.data.results;
-            $scope.setURL('', '');
-        }, function() {
-            alert("Error getting requests");
-            $scope.loadingData = false;
-        });
+        if (!campaign) {
+            $scope.showPopUp('warning', 'Your request is empty');   
+        } else {
+            $scope.loadingData = true;
+            var promise = $http.get("api/" + campaign + "/simple");
+            promise.then(function(data) {
+                if (!data.data.results.length) {
+                    $scope.showPopUp('warning', 'No such request');   
+                } else {
+                    if (campaign !== '' && add) {
+                        data.data.results.push.apply(data.data.results, $scope.allRequestData);
+                        $scope.arrCampaigns.push(campaign);
+                    } else {
+                        $scope.arrCampaigns = [campaign];
+                    }
+                }
+                $scope.loadingData = false;
+                $scope.$parent.allRequestData = data.data.results;
+                $scope.setURL('', '');
+            }, function() {
+                $scope.showPopUp('error', 'Error getting requests');
+                $scope.loadingData = false;
+            });
+        }
     };
     $scope.$parent.showview = false;
     $timeout(function() {$scope.nav('');}, 100);
 
     new ZeroClipboard(document.getElementById("copy"), {
         moviePath: '/lib/zeroclipboard/ZeroClipboard.swf'
-    }).on("ready", function(readyEvent) {
-
-        this.on("aftercopy", function(client, args) {
-            
-            console.log("Text copied");
-        });
     });
     $scope.setURL('', '');
 });
