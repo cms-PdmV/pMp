bd8ae2f353f9d9614300bacf0795045a5e8bccd2
AdrianAlan <adrianalan.pol@gmail.com>
2015-04-27 17:32:02 +0200

    adding multithreading with tornado

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from pmp import app
 
 ADMINS = ['cms-pdmv-serv@cern.ch']
 
 if __name__ == '__main__':
-
     if not app.debug:
         import logging
         from logging import Formatter
@@ -14,18 +16,24 @@ if __name__ == '__main__':
         mail_handler.setLevel(logging.ERROR)
         mail_handler.setFormatter(Formatter(
                 '''
-                Message type:       %(levelname)s
-                Location:           %(pathname)s:%(lineno)d
-                Module:             %(module)s
-                Function:           %(funcName)s
-                Time:               %(asctime)s
-                
-                Message:
-                    
-                %(message)s
+                Message type: %(levelname)s
+                Location: %(pathname)s:%(lineno)d
+                Module: %(module)s
+                Function: %(funcName)s
+                Time: %(asctime)s
+                Message: %(message)s
                 '''
                 ))
         app.logger.addHandler(mail_handler)
 
-    context = ('/home/crtkey/localhost.crt', '/home/crtkey/localhost.key')
+    settings = dict(
+        ssl_options = {
+            'certfile': '/home/crtkey/localhost.crt',
+            'keyfile': '/home/crtkey/localhost.key'
+            }
+        )
+
+    http_server = HTTPServer(WSGIContainer(app), **settings)
+    http_server.listen(443)
+    IOLoop.instance().start()
     app.run(host='0.0.0.0', port=443, ssl_context=context)
---
+++ run_dev.py
@@ -0,0 +1,6 @@
+from pmp import app
+
+ADMINS = ['cms-pdmv-serv@cern.ch']
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
