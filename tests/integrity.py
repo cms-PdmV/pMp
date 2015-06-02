c3384827ec9d6bf6276709f12aab759c00a6b10b
AdrianAlan <adrianalan.pol@gmail.com>
2015-06-02 11:48:44 +0200

    integrity tests playground

---
+++ tests/integrity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+class TestIntegrityEventsInDAS():
+
+    def __init__(self):
+        self.globalio = None
+
+    def run(self):
+        print "debug"
+        # get list of campaigns
+        # for each
+        # get number in historical
+        # get number in present
+        # log error
+
+
+if __name__ == "__main__":
+    tests = TestIntegrityEventsInDAS()
+    tests.run()
+    
