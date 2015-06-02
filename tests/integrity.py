906ee110dc02bbc8a627ce84bd61c9d8d6b41158
AdrianAlan <adrianalan.pol@gmail.com>
2015-06-02 13:30:15 +0200

    get numbers from announced

--- tests/integrity.py
+++ tests/integrity.py
@@ -2,16 +2,35 @@
 
 from datetime import datetime
 from pyelasticsearch import ElasticSearch
+from cStringIO import StringIO
 import logging
+import json
+import pycurl
 
 
 class TestIntegrityEventsInDAS():
 
     def __init__(self):
         self.db_url = 'http://127.0.0.1:9200'
+        self.pmp_api = 'http://127.0.0.1/api/'
         self.es = ElasticSearch(self.db_url)
         self.overflow = 1000
         self.setlog()
+
+    def curl(self, url, cookie=None):
+        out = StringIO()
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.URL, str(url))
+        curl.setopt(pycurl.WRITEFUNCTION, out.write)
+        curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+        curl.setopt(pycurl.SSL_VERIFYHOST, 0)
+        curl.perform()
+        try:
+            return (json.loads(out.getvalue()),
+                    curl.getinfo(curl.RESPONSE_CODE))
+        except Exception:
+            print "Status: %s/n%s" % (curl.getinfo(curl.RESPONSE_CODE),
+                                      out.getvalue())
 
     def setlog(self):
         logging.basicConfig(level=logging.INFO)
@@ -19,8 +38,13 @@ class TestIntegrityEventsInDAS():
     def get_historical(self, campaign):
         return 0
 
-    def get_present(self, campaign):
-        return 1
+    def get_announced(self, campaign):
+        details, status = self.curl(self.pmp_api + campaign +'/announced')
+        sum_events = 0
+        for request in details['results']:
+            if request['status'] == 'done':
+                sum_events += request['total_events']
+        return sum_events
 
     def run(self):
         # get list of campaigns
@@ -29,12 +53,11 @@ class TestIntegrityEventsInDAS():
                                     size=self.overflow)['hits']['hits']]
         for c in campaigns:
             if (self.get_historical(c['prepid']) !=
-                self.get_present(c['prepid'])):
+                self.get_announced(c['prepid'])):
                 logging.error(str(datetime.now()) + ' Inconsistency "events in'
                               + ' DAS" (historical) and status "done" (present)'
                               + ' for ' + c['prepid'])
         # get number in historical
-        # get number in present
 
 
 if __name__ == "__main__":
