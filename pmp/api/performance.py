9a086366e809a13c58db333b609e509a69ddc643
AdrianAlan <adrianalan.pol@gmail.com>
2015-07-01 15:26:47 +0200

    pylinting

--- pmp/api/performance.py
+++ pmp/api/performance.py
@@ -1,34 +1,34 @@
-from models import esadapter
+"""A list of classes supporting performance statistics"""
+from pmp.api.models import esadapter
 import json
 
 
 class PerformanceAPI(esadapter.InitConnection):
-    """
-    Return list of requests with history points
-    """
-    def get(self, campaign):
+    """Return list of requests with history points"""
 
+    def get(self, campaign):
+        """Retruning historical points for each request in given campaign"""
         # change 'all' to wildcard
         if campaign == 'all':
             campaign = '*'
 
         # get the list of requests
-        res = [s['_source'] for s in
-               self.es.search(('member_of_campaign:%s' % campaign),
-                              index='requests', size=self.overflow)
-               ['hits']['hits']]
+        response = [s['_source'] for s in
+                    self.es.search(('member_of_campaign:%s' % campaign),
+                                   index='requests', size=self.overflow)
+                    ['hits']['hits']]
 
         # loop over and remove documents' fields
-        for r in res:
+        for request in response:
             for field in ['time_event', 'total_events', 'completed_events',
                           'reqmgr_name', 'efficiency', 'output_dataset']:
-                if field in r:
-                    del r[field]
+                if field in request:
+                    del request[field]
 
             # duplicates fix ie. when request was reset
             patch_history = {}
-            for h in r['history']:
-                patch_history[h['action']] = h['time']
-            r['history'] = patch_history
+            for history in request['history']:
+                patch_history[history['action']] = history['time']
+            request['history'] = patch_history
 
-        return json.dumps({"results": res})
+        return json.dumps({"results": response})
