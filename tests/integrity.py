049fb06f71b25e86d4b9ab28c10327f496354695
AdrianAlan <adrianalan.pol@gmail.com>
2015-07-06 20:07:52 +0200

    pylinting tests

--- tests/integrity.py
+++ tests/integrity.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
-
+"""Integrity tests for pMp"""
 from datetime import datetime
-from pyelasticsearch import ElasticSearch
 from cStringIO import StringIO
 import logging
 import json
 import pycurl
+import pyelasticsearch
 import sys
 
 
-class TestIntegrityEventsInDAS():
+class TestIntegrityEventsInDAS(object):
+    """Check if present has same numbers as historical"""
 
     def __init__(self, arg):
         self.db_url = 'http://127.0.0.1:9200'
         self.pmp_api = 'http://127.0.0.1/api/'
-        self.es = ElasticSearch(self.db_url)
+        self.elastic_search = pyelasticsearch.ElasticSearch(self.db_url)
         self.overflow = 100000
         self.setlog()
         self.announced = arg
@@ -27,7 +27,9 @@ class TestIntegrityEventsInDAS():
             self.present_url = '/growing'
             self.historical_url = '/historical/3/,/all/all'
 
-    def curl(self, url, cookie=None):
+    @staticmethod
+    def curl(url):
+        """Perform curl"""
         out = StringIO()
         curl = pycurl.Curl()
         curl.setopt(pycurl.URL, str(url))
@@ -38,106 +40,118 @@ class TestIntegrityEventsInDAS():
         try:
             return (json.loads(out.getvalue()),
                     curl.getinfo(curl.RESPONSE_CODE))
-        except Exception:
+        except ValueError:
             print "Status: %s/n%s" % (curl.getinfo(curl.RESPONSE_CODE),
                                       out.getvalue())
 
-    def setlog(self):
+    @staticmethod
+    def setlog():
+        """Set logging level"""
         logging.basicConfig(level=logging.INFO)
 
     def get_historical(self, campaign):
-        details, status = self.curl(self.pmp_api + campaign
-                                    + self.historical_url)
+        """Get data from historical API"""
+        details, _ = self.curl(self.pmp_api + campaign + self.historical_url)
         if len(details['results']['data']):
             return details['results']['data'][-1]['e']
         else:
             return 0
 
     def get_announced(self, campaign):
+        """Get data from announced API"""
         sum_events = 0
         for request in self.get_requests(campaign):
             sum_events += request['total_events']
         return sum_events
 
     def get_requests(self, campaign):
-        details, status = self.curl(self.pmp_api + campaign + self.present_url)
+        """Get list of reqeusts on status done in a given campaign"""
+        details, _ = self.curl(self.pmp_api + campaign + self.present_url)
         for request in details['results']:
-            if (request['status'] == 'done'
-                and request['member_of_campaign'] == campaign):
+            if request['status'] == 'done' \
+                    and request['member_of_campaign'] == campaign:
                 yield request
 
     def completed_deep(self, request):
-        ce = 0
+        """Calculate true value of completed events"""
+        completed_events = 0
         if not len(request['output_dataset']):
             return 0
 
-        od = request['output_dataset'][0]
-        for rm in request['reqmgr_name']:
+        output_dataset = request['output_dataset'][0]
+        for reqmgr in request['reqmgr_name']:
             try:
-                stats = self.es.get('stats', 'stats', rm)['_source']
-            except:
+                stats = self.elastic_search.get('stats', 'stats',
+                                                reqmgr)['_source']
+            except pyelasticsearch.exceptions.ElasticHttpNotFoundError:
                 continue
 
-            if stats['pdmv_dataset_name'] == od:
-                s = stats['pdmv_monitor_history'][0]
-                ce = max(ce, s['pdmv_evts_in_DAS'] +
-                         s['pdmv_open_evts_in_DAS'])
+            if stats['pdmv_dataset_name'] == output_dataset:
+                sts = stats['pdmv_monitor_history'][0]
+                completed_events = max(completed_events,
+                                       sts['pdmv_evts_in_DAS'] +
+                                       sts['pdmv_open_evts_in_DAS'])
             elif 'pdmv_monitor_datasets' in stats:
-                for md in stats['pdmv_monitor_datasets']:
-                    if md['dataset'] == od:
-                        s = md['monitor'][0]
-                        ce = max(ce, s['pdmv_evts_in_DAS'] +
-                                 s['pdmv_open_evts_in_DAS'])
-        return ce
+                for monitor in stats['pdmv_monitor_datasets']:
+                    if monitor['dataset'] == output_dataset:
+                        field = monitor['monitor'][0]
+                        completed_events = max(completed_events,
+                                               field['pdmv_evts_in_DAS'] +
+                                               field['pdmv_open_evts_in_DAS'])
+        return completed_events
 
     def get_requests_grow(self, campaign):
-        requests = [s['_source'] for s in
-                     self.es.search(('member_of_campaign:%s' % campaign),
-                                    index='requests',
-                                    size=self.overflow)['hits']['hits']]
-        for r in requests:
-            if r['status'] == 'submitted':
-                r['completed_events'] = self.completed_deep(r)
-                yield r
+        """Get completed events for submitted requests"""
+        requests = [s['_source'] for s in \
+                        self.elastic_search.search( \
+                ('member_of_campaign:%s' % campaign), index='requests', \
+                    size=self.overflow)['hits']['hits']]
+        for request in requests:
+            if request['status'] == 'submitted':
+                request['completed_events'] = self.completed_deep(request)
+                yield request
 
     def run(self, deep_check=False):
+        """Run tests"""
         # get list of campaigns
-        campaigns = [s['_source'] for s in
-                     self.es.search('prepid:*', index='campaigns',
-                                    size=self.overflow)['hits']['hits']]
-        for c in campaigns:
-            logging.info('%s Checking %s' % (datetime.now(), c['prepid']))
-            if (self.get_historical(c['prepid']) !=
-                self.get_announced(c['prepid'])):
+        campaigns = [s['_source'] for s in \
+                         self.elastic_search.search( \
+                'prepid:*', index='campaigns', size=self.overflow)
+                     ['hits']['hits']]
+        for campaign in campaigns:
+            logging.info(str(datetime.now()) + " Checking " +
+                         campaign['prepid'])
+            if self.get_historical(campaign['prepid']) != \
+                    self.get_announced(campaign['prepid']):
                 logging.error(str(datetime.now()) + ' Inconsistency "events in'
                               + ' DAS" (historical) and status "done" (present'
-                              + ') for ' + c['prepid'])
+                              + ') for ' + campaign['prepid'])
                 if not deep_check:
                     continue
                 logging.info(str(datetime.now()) + ' Checking requests')
 
                 if self.announced:
-                    for request in self.get_requests(c['prepid']):
-                        if (request['total_events'] !=
-                            self.get_historical(request['prepid'])):
+                    for request in self.get_requests(campaign['prepid']):
+                        if request['total_events'] != \
+                                self.get_historical(request['prepid']):
                             logging.error(str(datetime.now()) +
                                           ' Inconsistency for ' +
                                           request['prepid'])
                 else:
-                    for request in self.get_requests_grow(c['prepid']):
-                        if (request['completed_events'] <  
-                            self.get_historical(request['prepid'])):
+                    for request in self.get_requests_grow(campaign['prepid']):
+                        if request['completed_events'] < \
+                                self.get_historical(request['prepid']):
                             logging.error(str(datetime.now()) +
                                           ' Inconsistency for ' +
                                           request['prepid'])
 
 
 if __name__ == "__main__":
-    deep = False
+    DEEP = False
     if len(sys.argv) > 1 and sys.argv[1] == "deep":
-        deep = True
-    tests = TestIntegrityEventsInDAS(True)
-    tests.run(deep)
-    tests = TestIntegrityEventsInDAS(False)
-    tests.run(deep)
+        DEEP = True
+    TESTS = TestIntegrityEventsInDAS(True)
+    TESTS.run(DEEP)
+    TESTS = TestIntegrityEventsInDAS(False)
+    TESTS.run(DEEP)
 
