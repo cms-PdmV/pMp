d9918f8e20d8b484340704b1be6c40ec194e6151
AdrianAlan <adrianalan.pol@gmail.com>
2015-07-15 14:22:55 +0200

    building and minifying js and css

--- pmp/Gruntfile.js
+++ pmp/Gruntfile.js
@@ -1,12 +1,54 @@
 module.exports = function(grunt) {
  grunt.initConfig({
-         concat: {
-             dist: {
-                 src: ['static/js/app.js', 'static/js/controllers.js', 'static/js/charts.js'],
-                 dest: 'static/js/pmp.build.min.js',
-             },
-         },
-     });
- 
- grunt.loadNpmTasks('grunt-contrib-concat')
+  concat: {
+   js: {
+    src: ['static/js/app.js', 'static/js/controllers.js'],
+    dest: 'static/build/pmp.build.js',
+   },
+   css: {
+    src: ['static/css/pmp.css'],
+    dest: 'static/build/pmp.build.css',
+   },
+  },
+  cssmin: {
+   options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+   },
+   target: {
+    files: {
+     'static/build/pmp.build.min.css': ['static/build/pmp.build.css']
+    }
+   }
+  },
+  uglify: {
+   js: {
+    src: ['static/build/pmp.build.js'],
+    dest: 'static/build/pmp.build.min.js'
+   },
+  },
+  watch: {
+   js: {
+    files: ['static/js/*.js'],
+    tasks: ['concat:js', 'uglify:js']
+   },
+   css: {
+    files: ['static/css/*.css'],
+    tasks: ['concat:css', 'cssmin']
+   },
+  },
+ });
+ grunt.loadNpmTasks('grunt-contrib-concat');
+ grunt.loadNpmTasks('grunt-contrib-cssmin');
+ grunt.loadNpmTasks('grunt-contrib-uglify');
+ grunt.loadNpmTasks('grunt-contrib-watch');
+ grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'watch'])
+ /*
+  *ToDo: Add htmlmin
+  *grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  */
+ /*
+  *ToDo: Add charts.js to js watch
+  *There are some problems after uglifying
+  */
 };
--- pmp/static/build/pmp.build.js
+++ pmp/static/build/pmp.build.js
@@ -1,3 +1,33 @@
+'use strict';
+
+var pmpApp = angular.module('pmpApp', ['ngAnimate', 'ngRoute', 'ui.bootstrap',
+                                       'pmpCharts', 'customFilters', 'customTags'])
+    .config(['$routeProvider', '$locationProvider',
+	 function($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'partials/index.html',
+                    controller: 'IndexController'
+                })
+                .when('/chains', {
+                    templateUrl: 'partials/chains.html',
+                    controller: 'ChainsController'
+                })
+	        .when('/historical', {
+		    templateUrl: 'partials/historical.html',
+                    controller: 'HistoricalController'
+		})
+	        .when('/performance', {
+		    templateUrl: 'partials/performance.html',
+                    controller: 'PerformanceController'
+		})
+                .when('/present', {
+                    templateUrl: 'partials/present.html',
+                    controller: 'PresentController'
+		});
+            $locationProvider.html5Mode(true);
+        }
+    ]);
 pmpApp.controller('MainController', ['$location', '$route', '$rootScope', '$interval', '$scope', '$timeout',
                                      function($location, $route, $rootScope, $interval, $scope, $timeout) {
                       
