7b1afda6a0a39fae1aadf206c27a64d2b0f2e2cd
AdrianAlan <adrianalan.pol@gmail.com>
2015-07-17 17:07:18 +0200

    added priority api

--- pmp/api/performance.py
+++ pmp/api/performance.py
@@ -1,6 +1,7 @@
 """A list of classes supporting performance statistics API"""
 from pmp.api.models import esadapter
 import json
+import time
 
 
 class PerformanceAPI(esadapter.InitConnection):
@@ -37,6 +38,62 @@ class PerformanceAPI(esadapter.InitConnection):
 class PriorityAPI(esadapter.InitConnection):
     """Return list of time/event statistics for different priority blocks"""
 
+    @staticmethod
+    def parse_time(string_time):
+        """Parse time in a "2013-1-01-01-01-01" format to integer"""
+        return time.mktime(time.strptime(string_time , "%Y-%m-%d-%H-%M"))*1000
+
+    @staticmethod
+    def fixed_priority_blocks(with_block):
+        for block, priority in enumerate([110000, 90000, 85000, 80000, 70000,
+                                          63000]):
+            if with_block:
+                yield str(block + 1), str(priority)
+            else:
+                yield str(priority)
+
     def get(self, campaign):
         """Execute"""
-        return json.dumps({"results": "dummy"})
+        # change 'all' to wildcard
+        if campaign == 'all':
+            campaign = '*'
+
+        # get the list of requests
+        response = [s['_source'] for s in
+                    self.es.search(('member_of_campaign:%s' % campaign),
+                                   index='requests', size=self.overflow)
+                    ['hits']['hits']]
+
+        com = {}
+        for request in response:
+            history = request['history']
+            last = len(history)-1
+            if (history[last]['action'] == 'done' and 
+                history[last-1]['action'] == 'submitted'):
+                request['stats'] = (self.parse_time(history[last]['time']) -
+                                    self.parse_time(history[last-1]['time']))
+
+            if not 'stats' in request:
+                continue
+
+            req_prio = str(request['priority'])
+
+            if req_prio in self.fixed_priority_blocks(False):
+                try:
+                    com[req_prio]['stats'] += request['stats']
+                    com[req_prio]['completed'] += request['completed_events']
+                except:
+                    com[req_prio] = dict()
+                    com[req_prio]['stats'] = request['stats']
+                    com[req_prio]['completed'] = request['completed_events']
+
+        response = dict()
+        for block, prio in self.fixed_priority_blocks(True):
+            try:
+                response["B" + block] = int(com[prio]['stats']/
+                                            com[prio]['completed'])
+            except:
+                pass
+            
+
+        return json.dumps({"results": response})
