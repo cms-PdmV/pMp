b582de5f218067b4e5174dc33cb664f9ca0e0524
AdrianAlan <adrianalan.pol@gmail.com>
2015-08-03 18:44:58 +0200

    changes for phone support

--- pmp/static/js/charts.js
+++ pmp/static/js/charts.js
@@ -822,17 +822,17 @@ angular.module('pmpCharts', [])
                     scope.$apply();
                 };
 
-                var innerHtml = "<style>.nav.dnd {margin-bottom: 0;}</style><div class='row'><div class='col-lg-9 col-md-12 col-sm-12' style='margin-bottom: 3px'><span class='col-lg-2 col-md-2 col-sm-2 nav-header text-muted'>selections</span><ul id='possible-selections' class='nav nav-pills dnd col-lg-10 col-md-10 col-sm-10 inline' style='min-height:22px'><li class='btn btn-default btn-xs text-uppercase' ng-repeat='value in selections'>{{value}}</li></ul></div>";
+                var innerHtml = "<style>.nav.dnd {margin-bottom: 0;}</style><div class='row'><div class='col-lg-9 col-md-12 col-sm-12 col-xs-12' style='margin-bottom: 3px'><span class='col-lg-2 col-md-2 col-sm-3 col-xs-5 nav-header text-muted'>selections</span><ul id='possible-selections' class='nav nav-pills dnd col-lg-10 col-md-10 col-sm-9 col-xs-7 inline' style='min-height:22px'><li class='btn btn-default btn-xs text-uppercase' ng-repeat='value in selections'>{{value}}</li></ul></div>";
 
                 for(var key in scope.difference) {
-                    innerHtml += "<div class='col-lg-6 col-md-12 col-sm-12'><span class='col-lg-3 col-md-2 col-sm-2 nav-header' style='margin-bottom: 3px'>" + key + "</span><ul id='" + key + "' class='nav nav-pills dnd single col-lg-9 col-md-10 col-sm-10 inline alert-info' style='min-height:23px; margin-top:1px'>";
+                    innerHtml += "<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'><span class='col-lg-3 col-md-4 col-sm-3 col-xs-5 nav-header' style='margin-bottom: 3px'>" + key + "</span><ul id='" + key + "' class='nav nav-pills dnd single col-lg-9 col-md-8 col-sm-9 col-xs-7 inline alert-info' style='min-height:23px; margin-top:1px'>";
                     if(scope.difference[key] !="") {
                         innerHtml+="<li class='btn btn-default btn-xs text-uppercase'>" + scope.difference[key] + "</li>";
                     }
                     innerHtml+="</ul></div>";
                 }
 
-                innerHtml += "<div class='col-lg-6 col-md-6 col-sm-12 spacing-sm' style='margin-top:3px;'><span class='col-lg-3 col-md-4 col-sm-2 nav-header'>scale</span><ul class='nav nav-pills inline col-lg-9 col-md-8 col-sm-10'><li><div class='btn-group'><button type='button' class='btn btn-primary btn-xs text-uppercase' ng-model='linearScale' ng-click='changeScale(true)' btn-radio='true'>linear</button><button type='button' class='btn btn-primary btn-xs text-uppercase' ng-model='logScale' ng-click='changeScale(false)' btn-radio='true'>log</button></div></li></ul></div></div>";
+                innerHtml += "<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12 spacing-sm' style='margin-top:3px;'><span class='col-lg-3 col-md-4 col-sm-3 col-xs-5 nav-header'>scale</span><ul class='nav nav-pills inline col-lg-9 col-md-8 col-sm-9 col-xs-7'><li><div class='btn-group'><button type='button' class='btn btn-primary btn-xs text-uppercase' ng-model='linearScale' ng-click='changeScale(true)' btn-radio='true'>linear</button><button type='button' class='btn btn-primary btn-xs text-uppercase' ng-model='logScale' ng-click='changeScale(false)' btn-radio='true'>log</button></div></li></ul></div></div>";
 
                 scope.changeScale = function(s) {
                     if (scope.linearScale != s) {
