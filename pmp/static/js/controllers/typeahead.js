172a4cfb709c3a570dc40da59f5fbe7a2310d58c
AdrianAlan <adrianalan.pol@gmail.com>
2015-08-19 17:11:29 +0200

    typeahead fix

--- pmp/static/js/controllers/typeahead.js
+++ pmp/static/js/controllers/typeahead.js
@@ -1,23 +1,9 @@
-angular.module('pmpApp').controller('TypeaheadCtrl', ['$scope', '$http', function($scope, $http) {
+angular.module('pmpApp').controller('TypeaheadController', ['$scope', '$http', 'PageDetailsProvider', function($scope, $http, PageDetailsProvider) {
     $scope.suggestions = [];
-    $scope.getSuggestions = function(query) {
+    $scope.getSuggestions = function(query, type) {
         if (query === '') return null;
-        if ($scope.title === 'Present: Announced Mode') {
-            $http.get('api/suggest/' + query + '/announced').then(function(response) {
-                $scope.suggestions = response.data.results;
-           });
-        } else if($scope.title === 'Present: Growing Mode') {
-            $http.get('api/suggest/' + query + '/growing').then(function(response) {
+        $http.get('api/suggest/' + query + type).then(function(response) {
                 $scope.suggestions = response.data.results;
             });
-        } else if ($scope.title === 'Historical Statistics') {
-            $http.get('api/suggest/' + query + '/historical').then(function(response) {
-                $scope.suggestions = response.data.results;
-            });
-        } else if ($scope.title === 'Request Performance') {
-            $http.get('api/suggest/' + query + '/performance').then(function(response) {
-                $scope.suggestions = response.data.results;
-            });
-        }
     };
 }]);
--- pmp/static/js/services/info.js
+++ pmp/static/js/services/info.js
@@ -1,9 +1,8 @@
 angular.module('pmpApp').factory('PageDetailsProvider', function() {
-    var pages = {
+    return {
         index: {id: 0},
-        present: {id: 1, title: 'Present Statistics', template: 'build/present.min.html'},
-        historical: {id: 2, title: 'Historical Statistics', template: 'build/historical.min.html'},
-        performance: {id: 3, title: 'Performance Statistics', template: 'build/performance.min.html'}
+        present: {id: 1, title: 'Present Statistics', template: 'build/present.min.html', typeahead: '/present'},
+        historical: {id: 2, title: 'Historical Statistics', template: 'build/historical.min.html', typeahead: '/historical'},
+        performance: {id: 3, title: 'Performance Statistics', template: 'build/performance.min.html', typeahead: '/performance'}
     };
-    return pages
 });
