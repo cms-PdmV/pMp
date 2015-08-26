88b6ed73b5fd30b85c0ef9ff29694dbaa2592048
AdrianAlan <adrianalan.pol@gmail.com>
2015-08-26 13:22:32 +0200

    jshinting, jesbeautifying services

--- pmp/static/js/services/data.js
+++ pmp/static/js/services/data.js
@@ -1,114 +1,199 @@
-angular.module('pmpApp').service('Data', ['$rootScope', function($rootScope) {
-        var filteredData = [], // currently displayed data (after filtering)
-            loadedData = [], // currently loaded data (before filtering)
-            inputTags = [], // input tags management
-            priorityFilter, statusFilter, pwgFilter; // filter details
-        return {
-            getFilteredData: function() {
-                return this.filteredData;
-            },
-            setFilteredData: function(i) {
-                this.filteredData = i;
-                $rootScope.$broadcast('onChangeNotification:FilteredData')
-            },
-            getInputTags: function() {
-                return this.inputTags;
-            },
-            setInputTags: function(i, append, remove) {
-                if (i === 'all') {
-                    for (var j = 0; j < this.loadedData.length; j++) {
-                        var input = this.loadedData[j].input;
-                        if (this.inputTags.indexOf(input) === -1) this.inputTags.push(input);
-                    }
-                } else if (append) {
-                    this.inputTags.push(i);
-                } else if (remove) {
-                    this.inputTags.splice(this.inputTags.indexOf(i), 1);
-                } else {
-                    this.inputTags = i;
+/**
+ * @name data.service
+ * @type service
+ * @description Service to share data between controllers.
+ */
+angular.module('pmpApp').service('Data', ['$rootScope', function ($rootScope) {
+    'use strict';
+    var filteredData = [], // currently displayed data (after filtering)
+        loadedData = [], // currently loaded data (before filtering)
+        inputTags = [], // input tags management
+        priorityFilter, statusFilter, pwgFilter; // filter details
+    return {
+        /**
+         * @description Filtered data getter.
+         * @return {Array} Array of filtered data objects.
+         */
+        getFilteredData: function () {
+            return this.filteredData;
+        },
+        /**
+         * @description Filtered data setter. Emits onChangeNotification.
+         * @params {Array} i the array of filtered data objects.
+         */
+        setFilteredData: function (i) {
+            this.filteredData = i;
+            $rootScope.$broadcast(
+                'onChangeNotification:FilteredData');
+        },
+        /**
+         * @description Input tags getter.
+         * @return {Array} String array of input tags.
+         */
+        getInputTags: function () {
+            return this.inputTags;
+        },
+        /**
+         * @description Input tags setter. Emits onChangeNotification.
+         * @params {Array} i the string array of input tags.
+         * @params {Boolean} append the tag is supposed to be added.
+         * @params {Boolean} remove the tag is supposed to be removed.
+         */
+        setInputTags: function (i, append, remove) {
+            if (i === 'all') {
+                for (var j = 0; j < this.loadedData.length; j++) {
+                    var input = this.loadedData[j].input;
+                    if (this.inputTags.indexOf(input) === -1)
+                        this.inputTags.push(input);
                 }
-                $rootScope.$broadcast('onChangeNotification:InputTags')
-            },
-            getLoadedData: function() {
-                return this.loadedData;
-            },
-            setLoadedData: function(i, append) {
-                if (append) Array.prototype.push.apply(i, this.loadedData);
-                this.loadedData = i;
-                $rootScope.$broadcast('onChangeNotification:LoadedData')
-            },
-            getPriorityFilter: function() {
-                return this.priorityFilter;
-            },
-            setPriorityFilter: function(i) {
-                this.priorityFilter = i;
-            },
-            getPWGFilter: function() {
-                return this.pwgFilter;
-            },
-            setPWGFilter: function(i) {
-                this.pwgFilter = i;
-            },
-            getStatusFilter: function() {
-                return this.statusFilter;
-            },
-            setStatusFilter: function(i) {
-                this.statusFilter = i;
-            },
-            changeFilter: function(data, reset, value, isStatusFilter) {
-                if (reset) {
-                    if(isStatusFilter) {
-                        this.statusFilter = {};
-                    } else {
-                        this.pwgFilter = {};
-                    }
-                }
-                var key;
-                for (var i = 0; i < data.length; i++) {
-                    if (isStatusFilter) {
-                        key = data[i].status; 
-                        if (this.statusFilter[key] === undefined) this.statusFilter[key] = value;
-                    } else {
-                        key = data[i].pwg;
-                        if (this.pwgFilter[key] === undefined) this.pwgFilter[key] = value;
-                    }
-                }
-            },
-            initializeFilter: function(data, isStatusFilter) {
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i] != '') {
-                        if (isStatusFilter) { 
-                            this.statusFilter[data[i]] = true;
-                        } else {
-                            this.pwgFilter[data[i]] = true;
-                        }
-                    }
-                }
-            },
-            reloadFilters: function(data) {
-                var iter, newStatus = {}, newPWG = {};
-                for (var i = 0; i < data.length; i++) {
-                    iter = data[i].status;
-                    if (newStatus[iter] === undefined) {
-                        newStatus[iter] = this.statusFilter[iter]
-                    }
-                    iter = data[i].pwg;
-                    if (newPWG[iter] === undefined) {
-                        newPWG[iter] = this.pwgFilter[iter]
-                    }
-                }
-                this.setStatusFilter(newStatus);
-                this.setPWGFilter(newPWG);
-                this.setLoadedData(data);
-            },
-            reset: function(ifFilter) {
-                this.loadedData = [];
-                this.setInputTags([], false, false);
-                if (filter) {
-                    this.priorityFilter = ['', ''];
+            } else if (append) {
+                this.inputTags.push(i);
+            } else if (remove) {
+                this.inputTags.splice(this.inputTags.indexOf(i),
+                    1);
+            } else {
+                this.inputTags = i;
+            }
+            $rootScope.$broadcast(
+                'onChangeNotification:InputTags');
+        },
+        /**
+         * @description Loaded data getter.
+         * @return {Array} Array of loaded data objects.
+         */
+        getLoadedData: function () {
+            return this.loadedData;
+        },
+        /**
+         * @description Loaded data setter. Emits onChangeNotification.
+         * @params {Array} i the Array of loaded data objects.
+         * @params {Boolean} append the array is supposed to be added instead of overwrite.
+         */
+        setLoadedData: function (i, append) {
+            if (append) Array.prototype.push.apply(i, this.loadedData);
+            this.loadedData = i;
+            $rootScope.$broadcast(
+                'onChangeNotification:LoadedData');
+        },
+        /**
+         * @description Priority filter getter.
+         * @return {Array} String array in a form [minimum, maximum].
+         */
+        getPriorityFilter: function () {
+            return this.priorityFilter;
+        },
+        /**
+         * @description Priority filter setter.
+         * @params {Array} i the string array in a form [minimum, maximum].
+         */
+        setPriorityFilter: function (i) {
+            this.priorityFilter = i;
+        },
+        /**
+         * @description PWG filter getter.
+         * @return {Object} PWG filter object in a form {pwg_name:{boolean}}.
+         */
+        getPWGFilter: function () {
+            return this.pwgFilter;
+        },
+        /**
+         * @description PWG filter setter.
+         * @params {Object} i the PWG filter object in a form {pwg_name:{boolean}}.
+         */
+        setPWGFilter: function (i) {
+            this.pwgFilter = i;
+        },
+        /**
+         * @description Status filter getter.
+         * @return {Object} Status filter object in a form {status_name:{boolean}}.
+         */
+        getStatusFilter: function () {
+            return this.statusFilter;
+        },
+        /**
+         * @description Status filter setter.
+         * @params {Object} i the Status filter object in a form {status_name:{boolean}}.
+         */
+        setStatusFilter: function (i) {
+            this.statusFilter = i;
+        },
+        /**
+         * @description Change filter object, status or pwg.
+         * @params {Array} data the loaded data array.
+         * @params {Boolean} reset the cleaning filter switch.
+         * @params {Boolean} value the default boolean assigned to new keys.
+         * @params {Boolean} isStatusFilter if true change status filter, otherwise change pwg.
+         */
+        changeFilter: function (data, reset, value, isStatusFilter) {
+            if (reset) {
+                if (isStatusFilter) {
                     this.statusFilter = {};
+                } else {
                     this.pwgFilter = {};
                 }
             }
+            var key;
+            for (var i = 0; i < data.length; i++) {
+                if (isStatusFilter) {
+                    key = data[i].status;
+                    if (this.statusFilter[key] === undefined)
+                        this.statusFilter[key] = value;
+                } else {
+                    key = data[i].pwg;
+                    if (this.pwgFilter[key] === undefined) this
+                        .pwgFilter[key] = value;
+                }
+            }
+        },
+        /**
+         * @description Initialize filter.
+         * @params {Array} data the array of keys to be set as true.
+         * @params {Boolean} isStatusFilter if true initialize status filter, otherwise change pwg.
+         */
+        initializeFilter: function (data, isStatusFilter) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i] !== '') {
+                    if (isStatusFilter) {
+                        this.statusFilter[data[i]] = true;
+                    } else {
+                        this.pwgFilter[data[i]] = true;
+                    }
+                }
+            }
+        },
+        /**
+         * @description When removing data, reload filters.
+         * @params {Array} data the loaded data array.
+         */
+        reloadFilters: function (data) {
+            var iter, newStatus = {},
+                newPWG = {};
+            for (var i = 0; i < data.length; i++) {
+                iter = data[i].status;
+                if (newStatus[iter] === undefined) {
+                    newStatus[iter] = this.statusFilter[iter];
+                }
+                iter = data[i].pwg;
+                if (newPWG[iter] === undefined) {
+                    newPWG[iter] = this.pwgFilter[iter];
+                }
+            }
+            this.setStatusFilter(newStatus);
+            this.setPWGFilter(newPWG);
+            this.setLoadedData(data);
+        },
+        /**
+         * @description Resets data objects shared in this service.
+         * @params {Boolean} ifFilter the reset filters marker.
+         */
+        reset: function (ifFilter) {
+            this.loadedData = [];
+            this.setInputTags([], false, false);
+            if (ifFilter) {
+                this.priorityFilter = ['', ''];
+                this.statusFilter = {};
+                this.pwgFilter = {};
+            }
         }
+    };
 }]);
--- pmp/static/js/services/info.js
+++ pmp/static/js/services/info.js
@@ -1,20 +1,65 @@
-angular.module('pmpApp').factory('PageDetailsProvider', function() {
+/**
+ * @name details.service
+ * @type service
+ * @description 
+ * Provides page details including title, template and typeahead call
+ * and error / warning messages
+ * @returns {Object} Page details object.
+ */
+angular.module('pmpApp').factory('PageDetailsProvider', function () {
+    'use strict';
     return {
-        index: {id: 0},
-        present: {id: 1, title: 'Present Statistics', template: 'build/present.min.html', typeahead: '/present'},
-        historical: {id: 2, title: 'Historical Statistics', template: 'build/historical.min.html', typeahead: '/historical'},
-        performance: {id: 3, title: 'Performance Statistics', template: 'build/performance.min.html', typeahead: '/performance'},
+        index: {
+            id: 0
+        },
+        present: {
+            id: 1,
+            title: 'Present Statistics',
+            template: 'build/present.min.html',
+            typeahead: '/present'
+        },
+        historical: {
+            id: 2,
+            title: 'Historical Statistics',
+            template: 'build/historical.min.html',
+            typeahead: '/historical'
+        },
+        performance: {
+            id: 3,
+            title: 'Performance Statistics',
+            template: 'build/performance.min.html',
+            typeahead: '/performance'
+        },
         messages: {
-            W0: {type: 'warning', message: 'Your request parameters are empty'},
-            W1: {type: 'warning', message: 'Your request is already loaded'},
-            W2: {type: 'warning', message: 'No results for this request parameters'},
-            W3: {type: 'warning', message: 'All data is filtered out'},
+            W0: {
+                type: 'warning',
+                message: 'Your request parameters are empty'
+            },
+            W1: {
+                type: 'warning',
+                message: 'Your request is already loaded'
+            },
+            W2: {
+                type: 'warning',
+                message: 'No results for this request parameters'
+            },
+            W3: {
+                type: 'warning',
+                message: 'All data is filtered out'
+            },
+            S0: {
+                type: 'success',
+                message: 'Succesfully loaded requests'
+            },
+            S1: {
+                type: 'success',
+                message: 'Succesfully appended requests'
+            },
 
-            S0: {type: 'success', message: 'Succesfully loaded requests'},
-            S1: {type: 'success', message: 'Succesfully appended requests'},
-
-            E0: {type: 'error', message: 'Error occured while getting requests'}
-            
+            E0: {
+                type: 'error',
+                message: 'Error occured while getting requests'
+            }
         }
     };
 });
