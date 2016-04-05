a9b4ce529f9db3b67382d0dfe31ba5cc9ee49ce8
Jacob Walker <jacob.walker@cern.ch>
2016-04-05 19:40:42 +0200

    Compact processing_string_provider.py again, hopefully simplifying flow a bit more

--- fetchd/processing_string_provider.py
+++ fetchd/processing_string_provider.py
@@ -28,29 +28,36 @@ class ProcessingStringProvider(object):
     def get(self, reqmgr_name):
         """Try getting a processing string and handle some of the common errors - raises
         NoProcessingString with a meaningful message if an error occurs"""
-        exception = None
-        status = 0 # in case an exception is thrown
-
         try:
             response, status = Utils.httpget(self._connection,
                 self._reqmgr_host + self._reqmgr_path + reqmgr_name)
         except httplib.HTTPException as ex1:
-            exception = ex1
+            logging.exception(Utils.get_time() + ' HTTP exception while getting ' + reqmgr_name
+                + ' from Request Manager')
 
             # Need to renew the connection for next time because an error in httplib can sometimes
             # leave the connection object in an invalid state
             self._connection = Utils.init_connection(self._reqmgr_host)
 
-        if exception is not None or status != 200:
             if self._use_backup:
-                response, status = self.get_from_backup(reqmgr_name)
-            else:
-                if exception is not None:
-                    raise NoProcessingString('A ' + type(exception).__name__ + ' exception'
-                        + ' occurred when fetching ' + reqmgr_name + ' from Request Manager')
-                else:
-                    raise NoProcessingString('An HTTP error ' + str(status) + ' was returned when'
-                        + ' fetching ' + reqmgr_name + ' from Request Manager')
+                logging.warning(Utils.get_time() + ' Contacting given backup Request Manager')
+
+                try:
+                    response, status = Utils.httpget(self._backup_connection,
+                        self._reqmgr_host_backup + self._reqmgr_path + reqmgr_name)
+                except httplib.HTTPException as ex2:
+                    logging.exception(Utils.get_time() + ' HTTP error while getting ' + reqmgr_name
+                        + ' from backup Request Manager')
+
+                    # Renewing the backup connection
+                    self._backup_connection = Utils.init_connection(self._reqmgr_host_backup)
+
+                    raise NoProcessingString('Errors occurred when fetching ' + reqmgr_name
+                        + ' from both given Request Managers')
+
+        if status > 0 and status != 200:
+            raise NoProcessingString('An HTTP error ' + str(status) + ' was returned when'
+                + ' fetching ' + reqmgr_name + ' from Request Manager')
         
         # By this point, errors should have been raised - assume we have some kind of result
         try:
@@ -61,19 +68,3 @@ class ProcessingStringProvider(object):
             raise NoProcessingString('Response from Request Manager did not contain a'
                 + ' processing string')
 
-    def get_from_backup(self, reqmgr_name):
-        """Use the backup connection to contact Request Manager""" 
-        logging.info(Utils.get_time() + ' Contacting given backup Request Manager')
-
-        try:
-            return Utils.httpget(self._backup_connection, self._reqmgr_host_backup +
-                self._reqmgr_path + reqmgr_name)
-        except httplib.HTTPException as ex2:
-            logging.exception(Utils.get_time() + ' HTTP error while getting ' + reqmgr_name
-                + ' from backup Request Manager')
-
-            # Renewing the backup connection
-            self._backup_connection = Utils.init_connection(self._reqmgr_host_backup)
-
-            raise NoProcessingString('Errors occurred when fetching ' + reqmgr_name
-                + ' from both given Request Managers')
